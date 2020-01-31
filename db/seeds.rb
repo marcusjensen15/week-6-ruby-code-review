6fc79e7d671c2b1276244aa8d3aec87b037a67e7
Marcus Jensen <marcusjensen1515@gmail.com>
2020-01-31 10:26:39 -0800

    modify seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+
+  def self.begin
+    seed = Seed.new
+    seed.generate_animals
+  end
+
+  def generate_animals
+    20.times do |i|
+      animal = Quote.create!(
+        name: Faker::FunnyName.three_word_name,
+        description: Faker::Hipster.paragraph(sentence_count: 2, supplemental: true),
+        type_of_animal: Faker::Creature::Animal.name,
+        age: rand(8)
+      )
+      puts "Animal #{i}: Name is #{animal.name} and their description is '#{animal.description}'."
+    end
+  end
+end
+
+Seed.begin
